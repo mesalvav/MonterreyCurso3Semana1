ed32e8708f9ca200b02f066582479c13c7328124
Mario Salvatierra <mesalvav@yahoo.com>
2016-06-30 20:55:15 -0400

    segundamente commit

--- Request/ViewController.swift
+++ Request/ViewController.swift
@@ -167,6 +167,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
  git init
  git add README.md
  git commit -m "first commit"
- git remote add origin https://github.com/mesalvav/MonterreyCurso3Semana1.git
+ git remote add origin https://github.com/mesalvav/MonterreyCurso3Semana2.git
  git push -u origin master
  */
